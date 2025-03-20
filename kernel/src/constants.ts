b9fd32a577412bf8cf6f53424e1aeee6022c17fc
igor.udot <horw7001@gmail.com>
2025-03-20 14:29:48 +0800

    use local bundle

--- kernel/src/constants.ts
+++ kernel/src/constants.ts
@@ -9,17 +9,13 @@ export const firmwareOptions: Record<string, FirmwareOption> = {
     name: 'Auto detection',
     url: ''
   },
-  'esp32': {
-    name: 'ESP32',
-    url: 'https://horw.github.io/buffer/ESP32_GENERIC-20241129-v1.24.1.bin'
-  },
   'esp32-c3': {
     name: 'ESP32-C3',
-    url: 'https://horw.github.io/buffer/ESP32_GENERIC_C3-20241129-v1.24.1.bin'
+    url: '../binaries/ESP32_GENERIC_C3-20241129-v1.24.1.bin'
   },
   'esp32-c6': {
     name: 'ESP32-C6',
-    url: 'https://horw.github.io/buffer/ESP32_GENERIC_C6-20241129-v1.24.1.bin'
+    url: '../binaries/ESP32_GENERIC_C6-20241129-v1.24.1.bin'
   }
 };
 
